707be4023625c3a396d288fc48f351927e433f78
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-08-05 08:59:14 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+
+//moveDodgerRight
+function moveDodgerRight() {
+
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left =parseInt(leftNumbers, 10)
+if (left<360) {
+    dodger.style.left = `${left+1}px`;
+
+}   
+}
+//moveDodgerLeft
+function moveDodgerLeft (){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left =parseInt(leftNumbers, 10)
+if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+
+}   
+}
+
+document.addEventListener("keydown", function (event) {
+    console.log(event);
+    if (event.key === "ArrowLeft") {
+        moveDodgerLeft()
+    }
+    else if (event.key === "ArrowRight") {
+        moveDodgerRight()
+    }
+});
