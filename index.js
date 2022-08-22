fb74bb2c8d906745be808898bef6e613842437d6
Rlynette <lynette.ronnoh@student.moringaschool.com>
2022-08-22 12:59:57 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 // Your code here
 
-//moveDodgerRight
-function moveDodgerRight() {
-
-    const leftNumbers = dodger.style.left.replace("px", "");
-    const left =parseInt(leftNumbers, 10)
-if (left<360) {
-    dodger.style.left = `${left+1}px`;
-
-}   
-}
 //moveDodgerLeft
-function moveDodgerLeft (){
+function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left =parseInt(leftNumbers, 10)
 if (left > 0) {
-    dodger.style.left = `${left - 1}px`;
+    dodger.style.left = `${left-1}px`;
+
+}   
+}
+//moveDodgerRight
+function moveDodgerRight (){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left =parseInt(leftNumbers, 10)
+if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
 
 }   
 }
 
-document.addEventListener("keydown", function (event) {
-    console.log(event);
-    if (event.key === "ArrowLeft") {
+document.addEventListener("keydown", function (e) {
+    console.log(e);
+    if (e.key === "ArrowLeft") {
         moveDodgerLeft()
     }
-    else if (event.key === "ArrowRight") {
+    else if (e.key === "ArrowRight") {
         moveDodgerRight()
     }
 });
